70a9ca298ed3f84970ac7c55772d19d9eb3ee92d
kaung-zaw-hein <kaungzawhein1963@gmail.com>
2025-05-24 14:42:05 +0100

    layout setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <main className='w-screen h-screen flex flex-col justify-center items-center py-10 px-5 bg-black'>
+        <h1 className=" text-5xl text-white  font-bold ">Press A Key to Start</h1>
+        <div class="grid grid-cols-2 gap-4 w-full h-full mx-auto mt-10">
+          <button id="green" class="bg-green-500 rounded-lg w-full h-full"></button>
+          <button id="red" class="bg-red-500 rounded-lg w-full h-full"></button>
+          <button id="yellow" class="bg-yellow-400 rounded-lg w-full h-full"></button>
+          <button id="blue" class="bg-blue-500 rounded-lg w-full h-full"></button>
+        </div>
+    </main>
   )
 }
 
